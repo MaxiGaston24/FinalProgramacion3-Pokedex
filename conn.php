0c80d6ebdc1dace5c43b28de117127eef27b52eb
Maxi Bazan <49814270+MaxiGaston24@users.noreply.github.com>
2021-07-30 21:05:17 -0300

    Add files via upload
    Acá esta el proyecto finalizado, está el backend y frontend

---
+++ conn.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbhost	= "localhost";	   // localhost or IP
+
+$dbuser	= "root";		  // database username
+
+$dbpass	= "";		     // database passwor
+
+$dbname	= "pokedex_db";    // database name
+
+?>
